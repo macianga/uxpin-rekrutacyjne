849e30279b8611fb7ed8126caca8add0e4657e45
maciek <macieq2048@gmail.com>
2022-02-10 23:18:46 +0100

    move footer to component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.scss'
 import {useEffect, useState} from "react";
 import butter from "./utils/butter-client";
+import Footer from "./components/navigation/Footer/Footer";
 
 type ButterCmsFieldsType = {
   header: string,
@@ -127,55 +128,7 @@ function App() {
           </span>
         </div>
       </div>
-      <div className="footer">
-        <div className="icon-medium uxpin-logo-white-fill"/>
-        <div className="footer-menu">
-          <div className="footer-single-menu">
-            <p>UXPIN</p>
-            <span/>
-            <span>Import from Sketch</span>
-            <span>Customers</span>
-            <span>Enterprise</span>
-            <span>etc.</span>
-          </div>
-          <div className="footer-single-menu">
-            <p>COMPARE</p>
-            <span/>
-            <span>UXPin vs Figma</span>
-            <span>UXPin vs Invision</span>
-            <span>UXPin vs ...</span>
-            <span>&nbsp;</span>
-          </div>
-          <div className="footer-single-menu">
-            <p>RESOURCES</p>
-            <span/>
-            <span>Docs</span>
-            <span>Video Tutorials</span>
-            <span>Blog</span>
-            <span>E-books</span>
-          </div>
-          <div className="footer-single-menu">
-            <p>ABOUT</p>
-            <span/>
-            <span>Company</span>
-            <span>Jobs</span>
-            <span>Press Kit</span>
-            <span>etc.</span>
-          </div>
-        </div>
-        <span className="copyright-text">
-          © UXPin Inc. All rights reserved.
-        </span>
-        <div className="social-and-terms">
-          <span>Privacy</span>
-          <span>Terms of Service</span>
-          <div className="social-media-logos">
-            <div className="icon-xsmall facebook-logo" style={{display: "inline-block"}}/>
-            <div className="icon-xsmall twitter-logo" style={{display: "inline-block"}}/>
-            <div className="icon-xsmall dribble-ball-logo" style={{display: "inline-block"}}/>
-          </div>
-        </div>
-      </div>
+      <Footer/>
     </div>
   )
 }
---
+++ src/components/navigation/Footer/Footer.tsx
@@ -0,0 +1,57 @@
+import "./footer.scss"
+
+function Footer() {
+  return (
+      <div className="footer">
+        <div className="icon-medium uxpin-logo-white-fill"/>
+        <div className="footer-menu">
+          <div className="footer-single-menu">
+            <p>UXPIN</p>
+            <span/>
+            <span>Import from Sketch</span>
+            <span>Customers</span>
+            <span>Enterprise</span>
+            <span>etc.</span>
+          </div>
+          <div className="footer-single-menu">
+            <p>COMPARE</p>
+            <span/>
+            <span>UXPin vs Figma</span>
+            <span>UXPin vs Invision</span>
+            <span>UXPin vs ...</span>
+            <span>&nbsp;</span>
+          </div>
+          <div className="footer-single-menu">
+            <p>RESOURCES</p>
+            <span/>
+            <span>Docs</span>
+            <span>Video Tutorials</span>
+            <span>Blog</span>
+            <span>E-books</span>
+          </div>
+          <div className="footer-single-menu">
+            <p>ABOUT</p>
+            <span/>
+            <span>Company</span>
+            <span>Jobs</span>
+            <span>Press Kit</span>
+            <span>etc.</span>
+          </div>
+        </div>
+        <span className="copyright-text">
+          © UXPin Inc. All rights reserved.
+        </span>
+        <div className="social-and-terms">
+          <span>Privacy</span>
+          <span>Terms of Service</span>
+          <div className="social-media-logos">
+            <div className="icon-xsmall facebook-logo" style={{display: "inline-block"}}/>
+            <div className="icon-xsmall twitter-logo" style={{display: "inline-block"}}/>
+            <div className="icon-xsmall dribble-ball-logo" style={{display: "inline-block"}}/>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default Footer
